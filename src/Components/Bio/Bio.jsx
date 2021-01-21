ecdeb6fa0b612353335c96bd90ac06dd4122b1d1
Beshayr <beshayr37@gmail.com>
2021-01-20 18:08:38 -0600

    creating component
    creating component files

---
+++ src/Components/Bio/Bio.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Bio.css';
+
+const Bio = () => {
+    
+    return (
+        <section id="bio" className="text-2xl tracking-widest">
+            <h1>BIO</h1>
+            <hr/>
+            <p>
+            I am looking for a position with a good startup for my career towards web development, expanding the knowledge base and picking up new concepts and technologies. Currently working at smile direct club in impression and attending Vanderbilt University's coding bootcamp to be a full stack web developer.
+            </p>                    
+        </section>
+    )
+
+}
+
+export default Bio;
---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Intro = () => {
+    
+    return (
+        <section id="intro" className="text-2xl tracking-widest">
+        <h1 className="-mb-1 pb-0">BESHAYR SAID</h1>
+        <h1 className="-my-1 py-0">FULL STACK WEB DEVELOPER</h1>
+        <h1>NASHVILL, TENNESSEE</h1>
+    </section>)
+}
+
+export default Intro;
