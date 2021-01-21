0fdb908cf455ffc90072ed478f3de4c4b0eccd3a
Beshayr <beshayr37@gmail.com>
2021-01-21 17:40:29 -0600

    Updates

---
+++ precache-manifest.e1452a3142a00a62ec06487efd3633a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d4257c56178043b9009e5a72b05068",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "76c52e02bf9174f2e51d",
+    "url": "/react-portfolio/static/css/main.47aa9d61.chunk.css"
+  },
+  {
+    "revision": "2d738c599136c649bb55",
+    "url": "/react-portfolio/static/js/2.220bf467.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-portfolio/static/js/2.220bf467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76c52e02bf9174f2e51d",
+    "url": "/react-portfolio/static/js/main.8cb7323b.chunk.js"
+  },
+  {
+    "revision": "65015130231404481b6d",
+    "url": "/react-portfolio/static/js/runtime-main.4191cdb0.js"
+  },
+  {
+    "revision": "5d8c71c656598a5b24dce1bd16c1e759",
+    "url": "/react-portfolio/static/media/resume.5d8c71c6.pdf"
+  }
+]);
